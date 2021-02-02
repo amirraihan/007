f31f25da49dc194b0f563159f73bca7867a425e4
syamila <ainmila07@gmail.com>
2021-02-02 15:02:32 +0800

    UPDATE LOGOUT FUNCTION

---
+++ GO LAUNDRY/app/src/main/java/com/example/GOLAUNDRY/homepage.java
@@ -0,0 +1,48 @@
+package com.example.GOLAUNDRY;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class homepage extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(homepage.this,Login.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
