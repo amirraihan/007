d36df2c5c6a41fe25b41e55f8c86f7c8fc5d4684
aafiqz <adamafiq345@gmail.com>
2021-02-28 22:43:10 +0800

    UPDATE HAMBURGER (ADA YG TK FUNCTION) DENGAN ABOUT US

--- GO LAUNDRY/app/src/main/java/com/example/GOLAUNDRY/homepage.java
+++ GO LAUNDRY/app/src/main/java/com/example/GOLAUNDRY/homepage.java
@@ -1,17 +1,28 @@
 package com.example.GOLAUNDRY;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class homepage extends AppCompatActivity {
+public class homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth firebaseAuth;
+    //Variable
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
 
     @Override
@@ -21,14 +32,27 @@ public class homepage extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
+        /*--Hooks-*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        //toolbar
+        setSupportActionBar(toolbar);
+
+        /*- Navigation Drawer Menu -*/
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
-    private void Logout(){
-        firebaseAuth.signOut();
-        finish();
-        startActivity(new Intent(homepage.this, Login.class));
-    }
-    @Override
+
+    /*-@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
@@ -48,5 +72,42 @@ public class homepage extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }-*/
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home: //home
+                startActivity(new Intent(homepage.this, homepage.class));
+                break;
+
+            case R.id.ProfileMenu://profile
+                startActivity(new Intent(homepage.this, profilePage.class));
+                break;
+
+            case R.id.nav_about:
+                startActivity(new Intent(homepage.this, AboutUs.class));
+                break;
+
+            /**-case R.id.nav_contact:
+                startActivity(new Intent(homepage.this, ContactUs.class));
+                break;-*/
+
+            case R.id.logoutMenu: //logout punya part
+                startActivity(new Intent(homepage.this, Login.class));
+                break;
+
+        }
+        return true;
     }
 }
